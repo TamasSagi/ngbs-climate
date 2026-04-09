781dd1e6a69560fce4c6017fa202c35c075bcf1b
Tamas Sagi <sagi.tamas93@gmail.com>
2026-04-09 10:49:42 +0200

    ci: :wrench: type annotation is now required, except in tests

--- ngbs-api/ngbs_api/validators.py
+++ ngbs-api/ngbs_api/validators.py
@@ -1,11 +1,11 @@
-from typing import Annotated, Any
+from typing import Annotated
 
 from pydantic import BeforeValidator
 
 from ngbs_api.types import CEMode, HCMode
 
 
-def try_convert_to_float(value: Any) -> float:
+def try_convert_to_float(value: float | int | str) -> float:
     try:
         return float(value)
 
@@ -13,7 +13,7 @@ def try_convert_to_float(value: Any) -> float:
         raise ValueError(f"Cannot convert type {type(value).__name__} ({value}) to float")
 
 
-def try_convert_to_bool(value: Any):
+def try_convert_to_bool(value: float | int | str) -> bool:
     match value:
         case str():
             return value.lower() in ("true", "1", "yes", "on")
@@ -32,7 +32,7 @@ def try_convert_to_cemode(value: int) -> CEMode:
     raise ValueError(f"Cannot convert type {type(value).__name__} ({value}) to CEMode")
 
 
-def try_convert_to_hcmode(value: Any) -> HCMode:
+def try_convert_to_hcmode(value: int) -> HCMode:
     if isinstance(value, int):
         return HCMode.HEATING if value == 0 else HCMode.COOLING
 
